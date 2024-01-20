42c6d2421491edc8aba3a21f882ca5309133bac0
GIGISL <155283059+GIGISL@users.noreply.github.com>
2024-01-20 19:39:21 +0900

    Add files via upload

---
+++ FindAPI.java
@@ -0,0 +1,14 @@
+public class FindAPI {
+    public static void main(String[] args) {
+        int a;
+        a = 10;
+        System.out.println("a = " + a);
+        //apple 문자열 저장하고 출력
+        String s= "APPLE";
+        System.out.println("s = " + s);
+        System.out.println(s.length());
+        System.out.println(s.toLowerCase());
+        System.out.println(s.charAt(2));
+
+    }
+}
---
+++ VDATest.java
@@ -0,0 +1,20 @@
+public class VDATest {
+    public static void main(String[] args){
+        int a = 10;
+        int b = a;
+        int c = b * 10;
+        int sum = 0;
+        for (int i = 1; i < 6; ++i){
+            sum=sum+i;
+        }
+        System.out.println(sum);
+        int x = 10;
+        int y = 20;
+        int exc = x;
+        x = y;
+        y = exc;
+        System.out.println("x = " + x);
+        System.out.println("y = " + y);
+
+    }
+}
